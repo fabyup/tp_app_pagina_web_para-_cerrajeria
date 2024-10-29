3e2b8f4cbb22f95e152be407801393bd4b5cc95c
fabyup <afabyblasco@gmail.com>
2024-10-29 11:39:36 -0300

    agregue this.imagen en la clase para que se vea la imagen

--- Servicio.js
+++ Servicio.js
@@ -6,45 +6,41 @@ class Servicio {
     this.imagen = imagen;
   }
 
-   generarHTML() {
+  generarHTML() {
     return `
       <li>
         <img src="${this.imagen}" alt="${this.nombre}" class="imagen-servicio">
         <strong>${this.nombre}</strong>: ${this.descripcion}
       </li>`;
   }
- 
 }
-    
-    
-
 
 const servicios = [
   new Servicio(
     "Apertura de puertas",
-    "Servicio rápido y eficiente para abrir cualquier tipo de puerta."
+    "Servicio rápido y eficiente para abrir cualquier tipo de puerta.",
     "istockphoto-1357874479-612x612.jpg"
   ),
   new Servicio(
     "Cambio de cerraduras",
-    "Instalación y cambio de cerraduras para mayor seguridad."
+    "Instalación y cambio de cerraduras para mayor seguridad.",
     "cambiar cerradura-imgEs20230315012353Peq.jpg"
   ),
   new Servicio(
     "Duplicado de llaves",
-    "Realizamos duplicados de llaves en el momento."
+    "Realizamos duplicados de llaves en el momento.",
     "keys-8877747_640.png"
   ),
-  
-  new Servicio("Apertura de puertas de autos",
-    "Trabajos con eficiencia."
-    "istockphoto-1137285203-612x612 apertura de autos.jpg"
 
+  new Servicio(
+    "Apertura de puertas de autos",
+    "Trabajos con eficiencia.",
+    "istockphoto-1137285203-612x612 apertura de autos.jpg"
   ),
-  
+
   new Servicio(
     "Reparación de cajas fuertes",
-    "Especialistas en reparación de cajas fuertes."
+    "Especialistas en reparación de cajas fuertes.",
     "3306927 caja fuerte.png"
   )
 
